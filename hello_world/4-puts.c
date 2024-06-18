52ba055b4f41d86f6e6d6ff8c214281be81e14d5
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-18 08:11:14 +0000

    answer tasks 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
 *main - First point
 *Return: Always 0.
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
