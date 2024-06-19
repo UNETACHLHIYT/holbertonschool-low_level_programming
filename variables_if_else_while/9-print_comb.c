ba11e3ce77112f0050b27e3731d24be382422d0b
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-19 11:03:36 +0000

    answer tasks9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - prints numbers with single-digit numbers
+ * main - prints numbers wiith single-digit numbers.
  * Return: 0
  */
 int main(void)
@@ -10,10 +10,16 @@ int main(void)
 	int numbers;
 
 	for (numbers = 0; numbers < 10; numbers++)
+
 	{
 		putchar(numbers + '0');
+		if (numbers < 9)
+		{
 		putchar(',');
+		putchar(32);
+		}
 }
+
 putchar('\n');
 return (0);
 }
