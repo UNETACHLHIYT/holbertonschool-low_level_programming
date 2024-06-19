97c9b224e25e1dc55ed7d595bfa01df799a82124
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-19 11:41:29 +0000

    answer tasks0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+int main(void) 
+{
+         printf("_putchar");
+        putchar('\n');
+        return (0);
+}
