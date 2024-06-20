35e9b9a79791cabfac22554b037f233a7607e167
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-20 12:52:56 +0000

    answertasks3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check character is low 0 1
+ * @c:charactere checked
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	_putchar('\n');
+	return (0);
+}
