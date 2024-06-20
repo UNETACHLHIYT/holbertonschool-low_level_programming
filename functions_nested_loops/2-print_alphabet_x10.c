c39925f10de4bfd7005b49e12c66fbd80a162078
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 <fatimazzahrabouch@gmail.com>
2024-06-20 11:18:36 +0000

    answertask2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercas x 10
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char print_alphabet10;
+	int print_alphabet;
+
+	for (print_alphabet = 0; print_alphabet <= 10; print_alphabet++)
+
+{
+	for (print_alphabet10 = 'a'; print_alphabet10 <= 'z'; print_alphabet10++)
+
+	{
+		_putchar(print_alphabet10);
+		{
+			_putchar('\n');
+}
+}
