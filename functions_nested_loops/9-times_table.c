9f225674f902a978c12e2c45246fe63802d79d51
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-21 22:24:48 +0000

    answertasks9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,28 +1,40 @@
 #include "main.h"
 /**
  * times_table - 9 times table
- * @num @result: print checked
+ * @num1 @num2 @result @final1 @final2: print checked
  * Return: 0
  */
 void times_table(void)
 {
-	int num;
-	int result;
-	int final;
-
-	for (num = 0; num <= 9; num++)
+int num1;
+int num2;
+int result;
+int final1;
+int final2;
+for (num1 = 0; num1 <= 9; num1++)
+{
+	for (num2 = 0; num2 <= 9; num2++)
 	{
-		for (result = 0; result <= 9; result++)
-		{
-			final = num * result;
-			if (result != 0)
+		result = num1 * num2
+			if (result > 9)
 			{
+				final1 = result % 10;
+				final2 = result / 10;
 				_putchar(',');
 				_putchar(' ');
-}
-_putchar('0' + final / 10);
-_putchar('0' + final % 10);
-}
-_putchar('\n');
+				_putchar(final2 + '0');
+				_putchar(final1 + '0');
+			} else
+			{
+				if (num2 != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+	}
+				_putchar(result + '0');
+	}
+	}
+		_putchar('\n');
 }
 }
