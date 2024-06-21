a7e464bd6374f7783b782771240bf8211830b3c7
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-21 22:50:12 +0000

    answertasks10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -  two integers
+ *@number1: first integers
+ *@number2: second integers
+ * Return: Always 0.
+ */
+int add(int number1, int number2)
+{
+
+{
+	return (number1 + number2);
+}
+}
