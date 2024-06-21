79cc7b62cb755b5206fd48eea8a0c36be0e2fee3
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-21 09:09:58 +0000

    answertasks6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Write function hat computes the absolute value
+ * Return: Always 0.
+ */
+int _abs(int num)
+{
+	if (num < 0)
+{
+	return (-num);
+}
+	else
+{
+	return (num);
+}
+}
