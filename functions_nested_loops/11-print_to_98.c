67f34fe1f35816c1eb9e59a48f841d53a3788eef
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-22 01:02:00 +0000

    answertask11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,39 +8,41 @@
 
 void print_to_98(int n)
 {
-	int num;
+int num;
 
-	while (n != 98)
-{
-		num = n;
-	if (num < 0)
-		{
-			_putchar('-');
-			num = -num;
-		}
-		if (num >= 10)
-{
-	if (num >= 100)
-	{
-		    _putchar((num / 100) + '0');
-		    num = num % 100;
-	}
-	_putchar((num / 10) + '0');
-}
-_putchar((num % 10) + '0');
-	if (n != 98)
-{
-		_putchar(',');
-		_putchar(' ');
-}
-if (n < 98)
-	n++;
-	else
-	n--;
-{
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
-}
-}
+    while (n != 98)
+    {
+        num = n;
+        if (num < 0)
+        {
+            _putchar('-');
+            num = -num;
+        }
+
+        if (num >= 10)
+        {
+            if (num >= 100)
+            {
+                _putchar((num / 100) + '0');
+                num = num % 100;
+            }
+            _putchar((num / 10) + '0');
+        }
+        _putchar((num % 10) + '0');
+
+        if (n != 98)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
+
+        if (n < 98)
+            n++;
+        else
+            n--;
+    }
+
+    _putchar('9');
+    _putchar('8');
+    _putchar('\n');
 }
