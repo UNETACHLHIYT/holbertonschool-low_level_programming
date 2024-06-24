ec31603283caa2d66b312db225ac043bf77a0f96
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-24 09:02:30 +0000

    answertask0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*checks for uppercase - prints the alphabet in uppercase
+*@c: character checked
+ * Return: 0
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
