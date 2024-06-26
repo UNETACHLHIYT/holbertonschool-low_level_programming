3d8c51d11f4bf949d64c1ef3c9490eb3a66b403d
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-26 09:41:02 +0000

    answertask4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string
+ *@s: string
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int t;
+	int i;
+
+	for (t = 0; s[t] != '\0'; t++)
+	{
+
+
+
+	}
+
+	for (i = t - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+}
+
+_putchar(' ');
+
+_putchar('\n');
+}
