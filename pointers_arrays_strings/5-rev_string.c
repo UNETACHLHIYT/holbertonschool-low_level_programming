4780fdb6112f84d01a53fe0bc033a7b27e87b5b8
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-26 10:18:53 +0000

    answertask5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ *Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int t;
+	int i;
+	int n;
+
+	for (t = 0; s[t] != '\0'; t++)
+{
+
+}
+
+for (i = 0; i < t / 2; i++)
+{
+	n = s[i];
+	s[i] = s[t - 1 - i];
+	s[t - 1 - i] = n;
+}
+
+}
+
