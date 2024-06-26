abb4d2c30617208d97def7b6bdafbc023b1074bb
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-26 13:38:17 +0000

    answertask7

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *@str: string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (str[c] != '\0')
+{
+	if (c  % 2 == 0)
+	{
+		_putchar(str[c]);
+	}
+		c++;
+
+
+}
+
+_putchar('\n');
+
+}
