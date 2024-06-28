2dd1bc6d0352f7764b39a0342759d0277f7d4098
UNRTACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-28 13:01:24 +0000

    answertask0

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,9 +8,19 @@
  */
 int main(void)
 {
-    char *str;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-    str = "0123456789";
-    puts_half(str);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
     return (0);
 }
