f136208be4991fe5fd3b27cbadbcfc980ce7b1f9
UNRTACHLHIYT <fatimazzahrabouch@gmail.com>
2024-06-28 07:59:19 +0000

    answertask8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 /**
  * print_array -  prints n
- * @*a: array
+ * @a: array
  * @n: number
- *
+ *The numbers displayed in the same order as they are stored in the array
  * Return: Always 0.
  */
 void print_array(int *a, int n)
