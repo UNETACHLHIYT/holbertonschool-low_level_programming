29f15e60de1d966823e0b75bf1ef92e7064d044a
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-07-04 10:21:15 +0000

    answertask0

---
+++ pointers_arrays_strings/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string,
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (s == NULL)
+{
+		return;
+}
+if (*s == '\0')
+{
+	_putchar('\n');
+	return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
