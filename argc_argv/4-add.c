1f3dcacf2397a8ebccc4d4d8f756fa9844792a79
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-07-05 20:32:34 +0000

    answertask4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,37 +7,44 @@
  * @num: number
  * Return: Always 0
  */
+#include <stdio.h>
+#include <stdlib.h>
+
+int positivenumber(char *str)
+{
+	int i = 0;
+	while (str[i])
+{
+        if (str[i] < '0' || str[i] > '9')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
-	if (argc <= 1)
-{
-	printf("0\n");
-	return (0);
-}
-int res = 0;
-int i, j;
-int num;
+    int sum = 0;
+    int num;
+    int i;
 
-for (i = 1; i < argc; ++i)
-{
+    if (argc == 1)
+    {
+        printf("0\n");
+        return 0;
+    }
 
-	for (j = 0; argv[i][j] != '\0'; ++j)
-{
-	if (argv[i][j] < '0' || argv[i][j] > '9')
-{
-	printf("Erreur\n");
-	return (1);
-}
-}
-num = atoi(argv[i]);
+    for (i = 1; i < argc; i++)
+    {
+        if (positivenumber(argv[i]))
+        {
+            printf("Error\n");
+            return 1;
+        }
+        num = atoi(argv[i]);
+        sum += num;
+    }
 
-if (num <= 0)
-{
-	printf("Erreur\n");
-	return (1);
-}
-res += num;
-}
-printf("%d\n", res);
-return (0);
+    printf("%d\n", sum);
+    return 0;
 }
