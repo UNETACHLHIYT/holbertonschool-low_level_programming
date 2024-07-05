0e99966004a1ff8c36b7872f957c94504779866c
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-07-05 15:16:19 +0000

    answertask0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print prg
+ * @argc: number arg
+ * @argv: vect arg
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc; 
+
+    printf("%s\n", argv[0]);
+    return (0);
+}
