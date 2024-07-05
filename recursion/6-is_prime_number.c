4921cdfb7764e60feb6f27e77f2854da23006f17
UNETACHLHIYT <fatimazzahrabouch@gmail.com>
2024-07-05 10:07:06 +0000

    answertask6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - prime number
+ * @n: number
+ *
+ * Return: Always 0
+ */
+int is_prime_number(int n)
+{
+    if (n <= 1)
+        return 0;
+    return is_prime_numberbis(n, 2);
+}
+
+/**
+ * is_prime_numberbis - verification div number
+ * @n: number
+ * @i: div
+ *
+ * Return: Always 0
+ */
+int is_prime_numberbis(int n, int i)
+{
+    if (i * i > n)
+        return 1;
+    if (n % i == 0)
+        return 0;
+    return is_prime_numberbis(n, i + 1);
+}
+
+
--- recursion/main.h
+++ recursion/main.h
@@ -6,3 +6,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_recursionbis(int n, int dep, int fin);
+int is_prime_number(int n);
+int is_prime_numberbis(int n, int i);
+
